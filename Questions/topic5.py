5c04bcdde15011e19c887bce3d00325e175a4108
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-10-27 10:52:21 +0530

    Update topic5.py

--- Questions/topic5.py
+++ Questions/topic5.py
@@ -38,18 +38,27 @@
 #     print("Please enter a string")
 # print(f"Reverse of {input_str} is {result_str}")
 
-# Count Even and Odd Numbers in a List: Given a list of integers, count how many are even and how many are odd using a loop.
-list_1 = [1, 2, 3, 4, 5, 6, 7]
-count_even = 0
-count_odd = 0
+# # Count Even and Odd Numbers in a List: Given a list of integers, count how many are even and how many are odd using a loop.
+# list_1 = [1, 2, 3, 4, 5, 6, 7]
+# count_even = 0
+# count_odd = 0
 
-for i in list_1:
-    if i % 2 == 0:
-        count_even += 1
-    else:
-        count_odd += 1
-print(f"Number of even numbers in list {list_1} is {count_even}")
-print(f"Number of odd numbers in list {list_1} is {count_odd}")
+# for i in list_1:
+#     if i % 2 == 0:
+#         count_even += 1
+#     else:
+#         count_odd += 1
+# print(f"Number of even numbers in list {list_1} is {count_even}")
+# print(f"Number of odd numbers in list {list_1} is {count_odd}")
 
-if list_1[6] == int:
-    print("number")
+# Intermediate Level
+# Sum of Digits of a Number: Input an integer and find the sum of its digits using a while loop.
+num3 = int(input("Enter a number : "))
+result_2 = 0
+while num3 > 0:
+    digit = num3 % 10
+    result_2 += digit
+    num3 //= 10
+print(f"Sum of digits of {num3} is {result_2}")
+
+# Check for Prime Number: Ask the user for a number and check if it’s a prime using a for loop.
