aa8826505403170dc1d7d06cc7833b5da5d913f3
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-11-14 22:10:03 +0530

    Update Project3.py

--- Projects/Project3.py
+++ Projects/Project3.py
@@ -9,3 +9,40 @@ if re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', input_email):
     print(f"Provider of input email {input_email} is {email_subparts[0]}")
 else:
     print("Please input correct email address.")
+
+# Password Strength Checker
+input_password = input("Enter a password : ")
+print(f"Length of password is {len(input_password)}")
+
+count_upper = count_lower = count_numbers = count_special = 0
+
+if len(input_password) > 7 :
+    for ch in input_password:
+        if ch.isupper():
+            count_upper += 1
+        elif ch.islower():
+            count_lower += 1
+        elif ch.isnumeric():
+            count_numbers += 1
+        else:
+            count_special += 1
+            
+    if 0 in (count_special, count_lower, count_numbers, count_upper):
+        print("Password status is easy")
+        
+    elif count_upper> 0 and count_lower > 0 and count_numbers > 0 and count_special > 0:
+        print("Password is Strong!!")
+        
+    else:
+        print("Password strength is medium")
+        
+    print(f'''
+    Count of 
+        Capital Alphabets - {count_upper}
+        Small Albhabets - {count_lower}
+        Numbers - {count_numbers}
+        Special - {count_special}
+          ''')
+    
+else:
+    print("Please enter password of length greater than 7")
